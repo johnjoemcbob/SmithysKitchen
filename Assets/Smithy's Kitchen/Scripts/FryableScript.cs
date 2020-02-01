ceea6a4cb49ba871f085d961616645eb9702b8d6
Caspar (Desktop) <caspar.vee@gmail.com>
2020-02-01 14:16:59 +0000

    Forgot some code in FryableScript
    I forgot to make overridable start/update lol

--- Assets/Smithy's Kitchen/Scripts/FryableScript.cs
+++ Assets/Smithy's Kitchen/Scripts/FryableScript.cs
@@ -5,13 +5,13 @@ using UnityEngine;
 public class FryableScript : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
+    public virtual void Start()
     {
         
     }
 
     // Update is called once per frame
-    void Update()
+    public virtual void Update()
     {
         
     }
