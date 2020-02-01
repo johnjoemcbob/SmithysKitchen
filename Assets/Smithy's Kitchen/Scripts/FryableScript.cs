197069a3757444db1c87c73750bf4bbbb6f37827
Caspar (Desktop) <caspar.vee@gmail.com>
2020-02-01 14:08:40 +0000

    Frying pan, steel block, sizzle
    Steel block goes sizzle when it hits the frying pan.

---
+++ Assets/Smithy's Kitchen/Scripts/FryableScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FryableScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public virtual void StartFry()
+    {
+
+    }
+
+    public virtual void StopFry()
+    {
+
+    }
+
+    public virtual void Fry()
+    {
+
+    }
+}
