6678b5483f3791b3b69e5ff2cd5a131f0e22ed54
Caspar (Desktop) <caspar.vee@gmail.com>
2020-02-13 18:13:50 +0000

    Soup Beginnings + Fizzy Drink
    Pourable fizzy drink is a thing, added some more soup pot stuff (one handed/stove burner)

---
+++ Assets/Smithy's Kitchen/Scripts/BottleOfLiquidScript.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BottleOfLiquidScript : MonoBehaviour
+{
+    public GameObject liquidStreamObj;
+    LineRenderer liquidLineRenderer;
+
+    VRTK.Prefabs.Interactions.Interactables.InteractableFacade facade;
+
+    bool isOpen = true; //Leave it true for now.
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        liquidLineRenderer = liquidStreamObj.GetComponent<LineRenderer>();
+
+        facade = GetComponent<VRTK.Prefabs.Interactions.Interactables.InteractableFacade>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.up.y <= 0.0f)
+        {
+            if (facade.IsGrabbed)
+                StartPouring();
+        }
+        else
+        {
+            StopPouring();
+        }
+
+        if (!facade.IsGrabbed)
+            StopPouring();
+    }
+
+    void FixedUpdate()
+    {
+        if (!liquidStreamObj.activeSelf)
+            return;
+
+        Vector3 dir = Vector3.up;
+        RaycastHit hit;
+
+        if (Physics.Raycast(liquidStreamObj.transform.position, -dir, out hit, 50))
+        {
+            Debug.DrawLine(liquidStreamObj.transform.position, hit.point);
+            liquidLineRenderer.SetPosition(0, liquidStreamObj.transform.position);
+            liquidLineRenderer.SetPosition(1, hit.point);
+
+            Debug.Log(hit.collider.gameObject.name);
+        }
+    }
+
+    void StartPouring()
+    {
+        liquidStreamObj.SetActive(true);
+    }
+
+    void StopPouring()
+    {
+        liquidStreamObj.SetActive(false);
+    }
+}
