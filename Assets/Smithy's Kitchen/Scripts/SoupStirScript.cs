f60d8fb6ddaeb5d3a5f654a2a958641a11178cf3
Caspar (Desktop) <caspar.vee@gmail.com>
2020-02-19 00:35:24 +0000

    Stir soup stuff
    More soup stirring. Now with actual rotation of soup

--- Assets/Smithy's Kitchen/Scripts/SoupStirScript.cs
+++ Assets/Smithy's Kitchen/Scripts/SoupStirScript.cs
@@ -10,6 +10,13 @@ public class SoupStirScript : MonoBehaviour
     int previousQuad = -1;
     int currentQuad = -1;
 
+    float targetRotateStr = 0.0f;
+    float currentRotateStr = 0.0f;
+    float strMAX = 40.0f;
+
+    float exitTimer = 0.0f;
+    float exitTimerMAX = 3.0f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,34 +26,48 @@ public class SoupStirScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        currentRotateStr = Mathf.Lerp(currentRotateStr, targetRotateStr, 0.01f);
+        soupObj.transform.Rotate(0.0f, currentRotateStr * 0.1f, 0.0f);
+
+        //Reset if time runs out
+        exitTimer -= Time.deltaTime;
+        if (exitTimer <= 0.0f)
+        {
+            currentQuad = -1;
+            previousQuad = -1;
+            targetRotateStr = Mathf.Lerp(targetRotateStr, 0.0f, 0.01f);
+        }
     }
 
     public void QuadEntered(int quad)
     {
-        if (currentQuad == -1)
-        {
-            //Begin
-        }
-        else if (currentQuad < quad)
+        if (currentQuad < quad)
         {
             //Clockwise?
             //Lerp the rotation to try and get that fun resistance effect...
+            targetRotateStr += 5.0f;
         }
         else if (currentQuad > quad)
         {
             //Anticlockwise?
+            targetRotateStr -= 5.0f;
         }
 
-        previousQuad = currentQuad;
+        if (targetRotateStr > strMAX)
+            targetRotateStr = strMAX;
+
+        if (targetRotateStr < -strMAX)
+            targetRotateStr = strMAX;
+
         currentQuad = quad;
-        Debug.Log("CURRENT QUAD: " + quad);
+        exitTimer = exitTimerMAX;
+
+        //Debug.Log("CURRENT QUAD: " + quad);
     }
 
     public void QuadExited(int quad)
     {
-        previousQuad = currentQuad;
-        currentQuad = -1;
+        previousQuad = quad;
         //if currentquad is equal to previous quad?
     }
 }
