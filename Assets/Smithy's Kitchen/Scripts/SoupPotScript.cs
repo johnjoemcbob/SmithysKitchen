ed32ef4d43e14d74537b651846df7f602397f6ec
Caspar (Desktop) <caspar.vee@gmail.com>
2020-02-19 17:19:19 +0000

    Bubbles + Demoscene fix
    Fixed the hob in demoscene, soup now makes bubbles

--- Assets/Smithy's Kitchen/Scripts/SoupPotScript.cs
+++ Assets/Smithy's Kitchen/Scripts/SoupPotScript.cs
@@ -4,13 +4,19 @@ using UnityEngine;
 
 public class SoupPotScript : MonoBehaviour
 {
+    public GameObject burnerObj;
     public GameObject soupObj;
     GameObject baseObj;
     GameObject flakeObj;
+    ParticleSystem bubbleSystem;
+    MeshRenderer soupRenderer;
 
     public float maxSoupHeight = 0.275f;
 
-    MeshRenderer soupRenderer;
+    bool isSnapped = false;
+    bool isHeated = false;
+    float soupTemperature = 0.0f;
+    float soupTempMAX = 20.0f;
 
     // Start is called before the first frame update
     void Start()
@@ -21,15 +27,94 @@ public class SoupPotScript : MonoBehaviour
         flakeObj = soupObj.transform.Find("SoupFlakes").gameObject;
         flakeObj.SetActive(false);
 
+        bubbleSystem = soupObj.transform.Find("BubbleParticles").GetComponent<ParticleSystem>();
+
         soupRenderer = baseObj.GetComponent<MeshRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (isSnapped && (burnerObj != null))
+        {
+            if (isHeated != burnerObj.activeSelf)
+            {
+                SetHeated(burnerObj.activeSelf);
+            }
+        }
 
+
+        //INCREASE SOUP TEMP ON HEAT / DECREASE ON COOL
+        if (isHeated)
+        {
+            soupTemperature += Time.deltaTime;
+
+            if (soupTemperature > soupTempMAX)
+            {
+                soupTemperature = soupTempMAX;
+            }
+        }
+        else
+        {
+            soupTemperature -= Time.deltaTime;
+
+            if (soupTemperature <= 0.0f)
+            {
+                soupTemperature = 0.0f;
+            }
+        }
+
+        //Modify with temp
+        if ((bubbleSystem != null) && (bubbleSystem.gameObject.activeSelf))
+        {
+            ParticleSystem.EmissionModule em = bubbleSystem.emission;
+            em.rateOverTimeMultiplier = soupTemperature;
+
+            //Disable if cold enough
+            if (soupTemperature == 0.0f)
+            {
+                bubbleSystem.gameObject.SetActive(false);
+            }
+        }
     }
 
+    //----------------------------------------------------------------
+    public void OnSnapped()
+    {
+        isSnapped = true;
+
+        if (burnerObj != null)
+        {
+            SetHeated(burnerObj.activeSelf);
+        }
+    }
+
+    //----------------------------------------------------------------
+    public void OnUnsnapped()
+    {
+        isSnapped = false;
+
+        SetHeated(false);
+    }
+
+    //----------------------------------------------------------------
+    void SetHeated(bool val)
+    {
+        if (isHeated == val)
+            return;
+
+        isHeated = val;
+
+        if (isHeated)
+        {
+            if (bubbleSystem != null)
+            {
+                bubbleSystem.gameObject.SetActive(true);
+            }
+        }
+    }
+
+    //----------------------------------------------------------------
     public void AddBase(float amount = 0.005f)
     {
         //Activate soup
@@ -49,14 +134,25 @@ public class SoupPotScript : MonoBehaviour
         flakeObj.transform.localPosition = baseObj.transform.localPosition;
     }
 
+    //----------------------------------------------------------------
     public void LerpBaseMaterial(Material newMaterial, float lerpRate = 0.05f)
     {
         if (!soupRenderer)
             return;
 
         soupRenderer.material.Lerp(soupRenderer.material, newMaterial, lerpRate);
+
+        //Change the bubbles!
+        if (bubbleSystem != null)
+        {
+            Color soupColour = soupRenderer.material.color;
+            soupColour.a = 1.0f;
+            ParticleSystem.MainModule pm = bubbleSystem.main;
+            pm.startColor = soupColour;
+        }
     }
 
+    //----------------------------------------------------------------
     public void AddChunks(Material chunkMaterial)
     {
         if (!flakeObj.activeSelf)
